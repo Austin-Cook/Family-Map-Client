06711558e6ea1d1bdbce947904523149d6bb2a1b
Austin-Cook <austin.cook.id@gmail.com>
2023-03-16 21:10:54 -0600

    Create settings layout

---
+++ app/src/main/java/com/example/familymapclient/feature/settings/SettingsActivity.java
@@ -0,0 +1,20 @@
+package com.example.familymapclient.feature.settings;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toolbar;
+
+import com.example.familymapclient.R;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+//        Toolbar toolbar = findViewById(R.id.settingsToolbar);
+//        setSupportActionBar(toolbar);
+    }
+}
